0ae87e2e2a97c1eb997c8c28abf28d0c86dddbf4
salivon <andrijsalivon@gmail.com>
2023-10-13 22:20:48 +0200

    shit

--- src/model/test.py
+++ src/model/test.py
@@ -1,1 +1,10 @@
+import cv2 
 from process import Loader
+
+ld = Loader("dataset/lomy/stepnylom_jpg","dataset/lomy/tvarnylom_jpg")
+ld.randomize()
+print(ld.get_length())
+for i in range(20):
+    
+    img, label = ld.get(i)
+    cv2.imshow(img)
