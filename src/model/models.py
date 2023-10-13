7510d7d77b715fa42fcc923fccbe3e51916d5d54
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-13 18:20:52 +0200

    added ConvNet

--- src/model/models.py
+++ src/model/models.py
@@ -81,3 +81,72 @@ class ConvNeuralNet(Universal):
         self.conv1 = nn.Conv2d(1, 3, (3, 3))
         self.pool1 = nn.MaxPool2d((2, 2))
         self.activation1 = nn.ReLU()
+
+        self.conv2 = nn.Conv2d(3, 6, (3, 3))
+        self.pool2 = nn.MaxPool2d((2, 2))
+        self.activation2 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(6, 16, (3, 3))
+        self.pool3 = nn.MaxPool2d((2, 2))
+        self.activation3 = nn.ReLU()
+
+        self.conv4 = nn.Conv2d(16, 32, (3, 3))
+        self.pool4 = nn.MaxPool2d((2, 2))
+        self.activation4 = nn.ReLU()
+
+        self.conv5 = nn.Conv2d(32, 32, (3, 3))
+        self.pool5 = nn.AvgPool2d((2, 2))
+        self.activation5 = nn.ReLU()
+
+        self.flat = nn.Flatten()
+
+        self.layer1 = nn.Linear(7488, 2048)
+        self.activation_l1 = nn.ReLU()
+
+        self.layer2 = nn.Linear(2048, 512)
+        self.activation_l2 = nn.ReLU()
+
+        self.layer3 = nn.Linear(512, 128)
+        self.drop3 = nn.Dropout(self.config["dropout"])
+        self.activation_l3 = nn.ReLU()
+
+        self.layer4 = nn.Linear(128, 32)
+        self.drop4 = nn.Dropout(self.config["dropout"])
+        self.activation_l4 = nn.ReLU()
+
+        self.layer5 = nn.Linear(32, 1)
+        self.activation_fin = nn.Sigmoid()
+
+    def forward(self, x):
+        #conv
+        x = self.conv1(x)
+        x = self.activation1(self.pool1(x))
+
+        x = self.conv2(x)
+        x = self.activation2(self.pool2(x))
+
+        x = self.conv3(x)
+        x = self.activation3(self.pool3(x))
+
+        x = self.conv4(x)
+        x = self.activation4(self.pool4(x))
+
+        x = self.conv5(x)
+        x = self.activation5(self.pool5(x))
+
+        x = self.flat(x)
+
+        #linear
+        x = self.activation_l1(self.layer1(x))
+
+        x = self.activation_l2(self.layer2(x))
+
+        x = self.drop3(self.layer4(x))
+        x = self.activation_l3(x)
+
+        x = self.drop4(self.layer4(x))
+        x = self.activation_l4(x)
+
+        x = self.activation_fin(self.layer5(x))
+
+        return x
