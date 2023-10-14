7bfc1e86fd8fc264cd9c6e13254751e68ee4ead5
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-14 11:54:56 +0200

    added ports to KNN

--- src/main.py
+++ src/main.py
@@ -48,7 +48,7 @@ def process_and_open_page():
     # run inference on neural net
     npimg = ld.resizing(selected_img)
     print(npimg.shape)
-    out = inference.infer_SVM(npimg)
+    out = inference.infer_KNN_hist(npimg)
     answerLablou.configure(text=out)
     show_page("page2")
 
--- src/model/inference.py
+++ src/model/inference.py
@@ -6,6 +6,24 @@ import cv2
 import sklearn
 from skimage.transform import resize
 
+#KNN
+def image_to_feature_vector(image, size=(64, 48)):
+    # resize the image to a fixed size, then flatten the image into
+    # a list of raw pixel intensities
+    return cv2.resize(image, size).flatten()
+
+
+def extract_color_histogram(image, bins=(8, 8, 8)):
+    # extract a 3D color histogram from the HSV color space using
+    # the supplied number of `bins` per channel
+    gray = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    hsv = cv2.cvtColor(gray, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
+                        [0, 128, 0, 256, 0, 256])
+    # handle normalizing the histogram if we are using OpenCV > 2.4.X
+    cv2.normalize(hist, hist)
+    return hist.flatten()
+
 def infer_SVM(X):
     # load model
     model = pickle.load(open("src/model/saved/SVM/SVC.pickle", "rb"))
@@ -17,6 +35,31 @@ def infer_SVM(X):
 
     # you can use loaded model to compute predictions
     y_hat = model.predict(X)[0]
+
+    if y_hat == 0:
+        return "štěpný lom"
+    elif y_hat == 1:
+        return "tvárný lom"
+    
+def infer_KNN_hist(X):
+    # load model
+    model = pickle.load(open("src/model/saved/KNN/KNNh.pickle", "rb"))
+
+    X = extract_color_histogram(X)
+    #X = X.reshape(-1, 1)
+    y_hat = model.predict([X])[0]
+
+    if y_hat == 0:
+        return "štěpný lom"
+    elif y_hat == 1:
+        return "tvárný lom"
+
+def infer_KNN_raw(X):
+    # load model
+    model = pickle.load(open("src/model/saved/KNN/KNNr.pickle", "rb"))
+
+    X = image_to_feature_vector(X)
+    y_hat = model.predict([X])[0]
 
     if y_hat == 0:
         return "štěpný lom"
