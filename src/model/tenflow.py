6fd763ae8271512a3bd6567db208ef0d43f5feb9
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-14 07:41:26 +0200

    80% test accuracy network

--- src/model/tenflow.py
+++ src/model/tenflow.py
@@ -8,6 +8,7 @@ from keras.utils import plot_model
 BATCH_SIZE = 8
 EPOCHS = 10
 
+
 wandb.init(
     # set the wandb project where this run will be logged
     project="LomyDetection",
@@ -26,10 +27,10 @@ wandb.init(
 print("Loading....")        
 loader = Loader("dataset/lomy/stepnylom_jpg","dataset/lomy/tvarnylom_jpg")
 
-#loader.generate_dataset(220)
+loader.generate_dataset(400)
 
-train_data = loader.get_array(0)[0:500]
-test_data = loader.get_array(0)[501:521]
+train_data = loader.get_array(1)
+test_data = loader.get_array(2)
 
 print("Loading done")
 
@@ -39,8 +40,8 @@ test_y = [i[1] for i in test_data]
 train_dataset = tf.data.Dataset.from_tensor_slices(([i[0]/255 for i in train_data], [i[1] for i in train_data]))
 test_dataset = tf.data.Dataset.from_tensor_slices((test_x, test_y))
 
-train_dataset = train_dataset.shuffle(8).batch(BATCH_SIZE)
-test_dataset = test_dataset.shuffle(8).batch(BATCH_SIZE)
+train_dataset = train_dataset.shuffle(69).batch(BATCH_SIZE)
+test_dataset = test_dataset.shuffle(69).batch(BATCH_SIZE)
 
 model = keras.models.Sequential()
 model.add(keras.layers.Input(batch_size=BATCH_SIZE,shape=(480,640,1)))
@@ -54,17 +55,18 @@ model.add(keras.layers.Conv2D(128, (3, 3), activation="relu"))
 model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(keras.layers.Conv2D(128, (3, 3), activation="relu"))
 model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Conv2D(64, (3, 3), activation="relu"))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
 
 model.add(keras.layers.Flatten())
 
-model.add(keras.layers.Dense(4096, activation="relu"))
+model.add(keras.layers.Dense(1024, activation="relu"))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(keras.layers.Dense(256, activation="relu"))
+model.add(tf.keras.layers.BatchNormalization())
 model.add(keras.layers.Dropout(0.2))
-model.add(keras.layers.Dense(2048, activation="relu"))
-model.add(keras.layers.Dense(512, activation="relu"))
-model.add(keras.layers.Dropout(0.2))
-model.add(keras.layers.Dense(128, activation="relu"))
-model.add(keras.layers.Dense(32, activation="relu"))
-model.add(keras.layers.Dropout(0.2))
+
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
