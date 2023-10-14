8541ca1f5e5a835c286ec0bd5660e017065941cc
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-14 07:48:44 +0200

    we too dum for wandb, lets become friends with matplotlib

--- src/model/tenflow.py
+++ src/model/tenflow.py
@@ -1,27 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
 from process import Loader
-import wandb
-from wandb.keras import WandbMetricsLogger
 from keras.utils import plot_model
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
 
 BATCH_SIZE = 8
 EPOCHS = 10
-
-
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="LomyDetection",
-
-    # track hyperparameters and run metadata with wandb.config
-    config={
-        "optimizer": "adam",
-        "loss": "binary_crossentropy",
-        "metric": "accuracy",
-        "epoch": EPOCHS,
-        "batch_size": BATCH_SIZE
-    }
-)
 
 
 print("Loading....")        
@@ -72,21 +58,30 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 model.summary()
 
-history = model.fit(train_dataset, epochs=EPOCHS, use_multiprocessing=False, callbacks=[WandbMetricsLogger()])
+history = model.fit(train_dataset, epochs=EPOCHS, use_multiprocessing=False)
 
-model.evaluate(test_dataset, callbacks=[WandbMetricsLogger()])
+model.evaluate(test_dataset)
 
 model.save("src/model/saved/NeuralNet/cnn.keras")
 plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
 
 print("model saved!")
 
-wandb.finish()
+plt.rcParams["figure.figsize"] = [12,4]
+figure, axis = plt.subplots(1, 2)
 
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_dataset.take(1):
-#     for i in range(9):
-#         ax = plt.subplot(3, 3, i + 1)
-#         plt.imshow(images[i].numpy().astype("uint8"))
-#         plt.title(int(labels[i]))
-#         plt.axis("off")
+axis[0].plot(history.history['loss'], label='loss- training data')
+#axis[0].plot(history.history['val_accuracy'], label='accuracy - validating data')
+axis[0].grid()
+axis[0].set_title('Loss')
+axis[0].legend()
+
+
+
+axis[1].plot(history.history['accuracy'], label='accuracy - training data')
+#axis[0].plot(history.history['val_accuracy'], label='accuracy - validating data')
+axis[1].grid()
+axis[1].set_title('Accuracy')
+axis[1].legend()
+
+plt.show()
