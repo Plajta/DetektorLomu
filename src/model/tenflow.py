a6f6cc9a0468067851cdbece53ab28a32bb376d9
HelloWorld7894 <danpojhan@protonmail.com>
2023-10-14 11:25:08 +0200

    added early stopping

--- src/model/tenflow.py
+++ src/model/tenflow.py
@@ -9,7 +9,6 @@ matplotlib.use("TkAgg")
 BATCH_SIZE = 8
 EPOCHS = 20
 
-
 print("Loading....")        
 loader = Loader("dataset/lomy/stepnylom_jpg","dataset/lomy/tvarnylom_jpg")
 
@@ -31,6 +30,9 @@ test_dataset = tf.data.Dataset.from_tensor_slices((test_x, test_y))
 
 train_dataset = train_dataset.shuffle(69).batch(BATCH_SIZE)
 test_dataset = test_dataset.shuffle(69).batch(BATCH_SIZE)
+
+#Early stopping
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 
 model = keras.models.Sequential()
 model.add(keras.layers.Input(batch_size=BATCH_SIZE,shape=(480,640,1)))
@@ -61,7 +63,8 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 model.summary()
 
-history = model.fit(train_dataset, epochs=EPOCHS, use_multiprocessing=False, validation_data=test_dataset)
+history = model.fit(train_dataset, epochs=EPOCHS, use_multiprocessing=False, validation_data=test_dataset,
+                    callbacks=[callback])
 model.evaluate(test_dataset)
 
 model.save("src/model/saved/NeuralNet/cnn.keras")
